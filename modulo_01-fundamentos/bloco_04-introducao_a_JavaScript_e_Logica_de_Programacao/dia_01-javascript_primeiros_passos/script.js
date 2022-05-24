ac6fc987517b943d47e6b48272ca756c57bd33d3
Junior S. Gomes <gomes@pop-os.localdomain>
2022-05-24 15:19:42 -0300

    Inicio do bloco 4

---
+++ modulo_01-fundamentos/bloco_04-introducao_a_JavaScript_e_Logica_de_Programacao/dia_01-javascript_primeiros_passos/script.js
@@ -0,0 +1,16 @@
+const myName = "Junior";
+const birthCity = "Duque de Caxias";
+let birthYear = 1993;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Rio de Janeiro";
+// Uma constante não pode ter seu valor reatribuído, por isso gera o erra na última saída.
+
+console.log(birthCity);
