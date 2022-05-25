2ce707e871c217972afefbd7bd0eb8fd496d5df0
Junior S. Gomes <gomes@pop-os.localdomain>
2022-05-25 17:49:52 -0300

    Adiciona o exercício_3

---
+++ modulo_01-fundamentos/bloco_04-introducao_a_JavaScript_e_Logica_de_Programacao/dia_01-javascript_primeiros_passos/exercises/exercise_03.js
@@ -0,0 +1,14 @@
+const valueA = 19;
+const valueB = 31;
+const valueC = 27;
+
+if ( valueA > valueB && valueA > valueC){
+  highestValue = valueA;
+} else if (valueB > valueC) {
+  highestValue = valueB;
+} else {
+  highestValue = valueC;
+}
+
+
+console.log(highestValue);
