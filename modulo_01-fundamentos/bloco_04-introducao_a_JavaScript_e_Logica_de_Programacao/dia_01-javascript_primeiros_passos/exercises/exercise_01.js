23845e70db062b6e9fc3903e2e5bc8ad77473415
Junior S. Gomes <gomes@pop-os.localdomain>
2022-05-25 17:24:13 -0300

    Adiciona os exercícios 1 e 2

---
+++ modulo_01-fundamentos/bloco_04-introducao_a_JavaScript_e_Logica_de_Programacao/dia_01-javascript_primeiros_passos/exercises/exercise_01.js
@@ -0,0 +1,26 @@
+const valorA = 12;
+const valorB = 3;
+
+//adição:
+let adicao = (valorA + valorB);
+
+//Subtração
+let subtracao = (valorA - valorB);
+
+//Multiplicação
+
+let multiplicacao = (valorA * valorB);
+
+// Divisão
+
+let divisao = (valorA / valorB);
+
+// Módulo
+
+let modulo = (valorA % valorB);
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
