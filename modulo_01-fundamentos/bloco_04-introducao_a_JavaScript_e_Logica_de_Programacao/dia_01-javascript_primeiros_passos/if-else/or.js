ff0ce649d1236e313c2805b0c7bd24dba7f50d44
Junior S. Gomes <gomes@pop-os.localdomain>
2022-05-24 22:49:14 -0300

    Adiciona a resolução dos exercícios propostos no conteúdo do dia 1 - bloco 04

---
+++ modulo_01-fundamentos/bloco_04-introducao_a_JavaScript_e_Logica_de_Programacao/dia_01-javascript_primeiros_passos/if-else/or.js
@@ -0,0 +1,12 @@
+/* console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false */
+
+let weekDay = "quarta-feira";
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" ||  weekDay === "quarta-feira" || weekDay === "quinta-feita" || weekDay === "sexta-feira"){
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}else{
+  console.log("FINALMENTE, descanso merecido UwU");
+}
