a1c7ef392e6d17eb85e5147f18ccbf06bd6a34f8
Junior S. Gomes <gomes@pop-os.localdomain>
2022-05-25 22:24:57 -0300

    Adiciona o exercício 8

---
+++ modulo_01-fundamentos/bloco_04-introducao_a_JavaScript_e_Logica_de_Programacao/dia_01-javascript_primeiros_passos/exercises/exercise_08.js
@@ -0,0 +1,11 @@
+const numberA = 22;
+const numberB = 77;
+const numberC = 61;
+
+let evenOdd = false;
+
+if (numberA % 2 === 0 || numberB % 2 === 0 || numberC % 2 === 0) {
+  evenOdd = true;
+}
+
+console.log(evenOdd);
